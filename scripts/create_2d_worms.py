e680584a556b1ff1752eaac03c082f15f5e4558d
lmanan <manan.lalit@gmail.com>
2023-09-28 16:22:18 -0400

    refactor code

--- scripts/create_2d_worms.py
+++ scripts/create_2d_worms.py
@@ -1,27 +1,26 @@
-
-import numpy as np
-from tqdm import tqdm
 from argparse import ArgumentParser
 
-if __name__=='__main__':
+if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument('--worm_dimension', 
-                        dest="worm_dimension", 
-                        type = int, 
-                        default = 2, 
-                        help='worm lives in n-D space')
-    parser.add_argument('--worm_segments', 
-                        dest="worm_segments", 
-                        type = int,
-                        default = 5, 
-                        help='number of segments in the worm body')
-    parser.add_argument('--number_worms', 
-                        dest="number_worms", 
-                        type = int,
-                        default = 1e3, 
-                        help='number of simulated worms')
+    parser.add_argument(
+        "--worm_dimension",
+        dest="worm_dimension",
+        type=int,
+        default=2,
+        help="worm lives in n-D space",
+    )
+    parser.add_argument(
+        "--worm_segments",
+        dest="worm_segments",
+        type=int,
+        default=5,
+        help="number of segments in the worm body",
+    )
+    parser.add_argument(
+        "--number_worms",
+        dest="number_worms",
+        type=int,
+        default=1e3,
+        help="number of simulated worms",
+    )
     args = parser.parse_args()
-
-
-
-
